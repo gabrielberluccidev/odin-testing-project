c3ccf619c02f4b263dc4b22220f45653a23fa6dd
Gabriel Berlucci <gabrielberluccidev@proton.me>
2025-10-28 13:28:02 -0300

    feat: add test for caesarcipher

--- functions/main.test.js
+++ functions/main.test.js
@@ -1,4 +1,9 @@
-import { capitalize, reverseString, calculation } from './main.js';
+import {
+  capitalize,
+  reverseString,
+  calculation,
+  caesarCipher,
+} from './main.js';
 import { expect, test } from '@jest/globals';
 
 // test('sol will be Sol', () => {
@@ -17,18 +22,22 @@ import { expect, test } from '@jest/globals';
 //   expect(reverseString('oi')).toBe('io');
 // });
 
-test('1.5 + 2 = 3.5', () => {
-  expect(calculation.add(1, 2)).toBe(3);
-});
+// test('1.5 + 2 = 3.5', () => {
+//   expect(calculation.add(1, 2)).toBe(3);
+// });
 
-test('1 - 2 = -1', () => {
-  expect(calculation.substract(1, 2)).toBe(-1);
-});
+// test('1 - 2 = -1', () => {
+//   expect(calculation.substract(1, 2)).toBe(-1);
+// });
 
-test('1 * 2 = 2', () => {
-  expect(calculation.multiply(1, 2)).toBe(2);
-});
+// test('1 * 2 = 2', () => {
+//   expect(calculation.multiply(1, 2)).toBe(2);
+// });
 
-test('10 / 2 = 5', () => {
-  expect(calculation.divide(10, 2)).toBe(5);
+// test('10 / 2 = 5', () => {
+//   expect(calculation.divide(10, 2)).toBe(5);
+// });
+
+test('xFWHU => EMDOB', () => {
+  expect(caesarCipher('xFwHU', 7)).toBe('eMdOB');
 });
